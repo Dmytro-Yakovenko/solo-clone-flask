8a50fca4958114ce971ae1c2772ff32d728b94e5
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2023-07-26 19:16:34 -0700

    about page

--- react-app/src/components/PinsPage/index.js
+++ react-app/src/components/PinsPage/index.js
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import Navigation from "../Navigation";
+import "./PinsPage.css";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPins } from "../../store/pinReducer";
+import { NavLink } from "react-router-dom";
 const PinsPage = () => {
-  return (
-    <div>PinsPage</div>
-  )
-}
+  const pins = useSelector((state) => Object.values(state.pins.pins));
+  console.log(pins, 33333333);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllPins());
+  }, [dispatch]);
 
-export default PinsPage
+  return (
+    <>
+      <Navigation />
+      <div className="container pins-page">
+        <h1 className="pins-page-title">Get your next dinner idea </h1>
+
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 3, 900: 5 }}>
+          <Masonry columnsCount={5}>
+            {pins.map((item) => (
+              <NavLink to={`/pins/${item.id}`} key={item.alt} className="pins-page-link">
+           
+    <img className="pins-images" src={item.images} alt={item.title} />
+           
+              
+                <h4>{item.title}</h4>
+              </NavLink>
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      </div>
+    </>
+  );
+};
+
+export default PinsPage;
